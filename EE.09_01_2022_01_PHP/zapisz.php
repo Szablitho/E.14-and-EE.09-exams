26e4729c3741e2841f8bd213ce174621960af871
Szablitho <108629608+Szablitho@users.noreply.github.com>
2022-07-03 22:54:06 +0200

    Delete EE.09_01_2022_01_PHP directory

--- EE.09_01_2022_01_PHP/zapisz.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$db = new mysqli('localhost','root','','wedkarstwo2022_v01_styczen');
-	$query=$db->prepare("INSERT INTO `karty_wedkarskie` (`imie`, `nazwisko`, `adres`, `data_zezwolenia`, `punkty`) VALUES (?, ?, ?, NULL, NULL);");
-	$query->bind_param("sss",$_POST['imie'],$_POST['nazwisko'],$_POST['tekst']);
-	$query->execute();
-	
-?>
